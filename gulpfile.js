2e349e2b53ddbb4c2c36517275ebc66bc7848ab6
Adam Armour <adam.armour@gmail.com>
2015-11-11 10:25:19 -0700

    chore: configure docs generation

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,32 @@ const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const nano = require('gulp-cssnano');
+const browserSync = require('browser-sync');
+const spawn = require('child_process').spawn;
+
+gulp.task('build-docs', (done) => {
+  browserSync.notify('Running: <kbd>$ jekyll build</kbd>');
+  spawn('jekyll', ['build'], { stdio: 'inherit' })
+    .on('close', done);
+});
+
+gulp.task('rebuild-docs', ['build-docs'], () => browserSync.reload());
+
+gulp.task('browser-sync', ['sass', 'build-docs'], () => {
+  browserSync({
+    server: {
+      baseDir: '_gh_pages'
+    }
+  });
+});
 
 gulp.task('sass', () => {
   gulp.src('./scss/elements.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer]))
     .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./_gh_pages/css'))
+    .pipe(browserSync.reload({ stream: true }))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(nano())
     .pipe(gulp.dest('./dist'));
@@ -17,6 +37,7 @@ gulp.task('sass', () => {
 
 gulp.task('watch', () => {
   gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch(['./docs/**/*.html', './docs/**/*.md'], ['rebuild-docs']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['browser-sync', 'watch']);
