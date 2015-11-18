b8f54c7bd5ca2931066a5b8f4746b99d2f2759c6
Adam Armour <adam.armour@gmail.com>
2015-11-18 13:14:15 -0700

    chore(npm_scripts/exec): fix stdio argument
    Fix the stdio option for execSync so that output is written to the terminal.

--- npm_scripts/exec.js
+++ npm_scripts/exec.js
@@ -1,5 +1,5 @@
 const execSync = require('child_process').execSync;
 
 module.exports = function exec(command) {
-  execSync(command, { stdio: 'inherit' });
+  execSync(command, { stdio: [0, 1, 2] });
 };
