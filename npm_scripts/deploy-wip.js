829346721f64841d9684148133313f537a6ffede
Adam Armour <adam.armour@gmail.com>
2015-12-09 14:59:01 -0700

    chore(npm_scripts): add -y option to deploy-wip

--- npm_scripts/deploy-wip.js
+++ npm_scripts/deploy-wip.js
@@ -2,22 +2,31 @@ const exec = require('./exec');
 const readline = require('readline');
 const path = require('path');
 
-const deployPath = path.join(process.cwd(), '_gh_pages');
-const currentBranch = exec('git rev-parse --abbrev-ref HEAD', true);
-
-const stdin = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-stdin.question(`Branch name (${currentBranch}): `, (branchName) => {
-  branchName = branchName || currentBranch;
-
+function deploy(branchName) {
+  const deployPath = path.join(process.cwd(), '_gh_pages');
   const domain = `pearson-elements-${branchName}.surge.sh`;
 
   console.log(`Deploying ${deployPath} to ${domain}`);
 
   exec(`surge -p ${deployPath} -d ${domain}`);
+}
 
-  stdin.close();
-});
+function main(argv) {
+  const currentBranch = exec('git rev-parse --abbrev-ref HEAD', true);
+
+  if (argv[0] === '-y') {
+    deploy(currentBranch);
+  } else {
+    const stdin = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    stdin.question(`Branch name (${currentBranch}): `, (branchName) => {
+      deploy(branchName || currentBranch);
+      stdin.close();
+    });
+  }
+}
+
+main(process.argv.slice(2));
