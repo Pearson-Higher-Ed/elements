88effd25c53a71fb814c6088aadd103a2a56f6ee
Adam Armour <adam.armour@gmail.com>
2015-12-15 09:21:29 -0700

    chore(scripts): skip git hooks when committing gh-pages branch

--- npm_scripts/gh-pages.js
+++ npm_scripts/gh-pages.js
@@ -4,5 +4,5 @@ exec('gulp clean');
 exec('NODE_ENV=gh-pages gulp build-docs');
 exec('git checkout gh-pages');
 exec('git add -f _gh_pages');
-exec('git commit -m "update gh-pages"');
+exec('git commit --no-verify -m "update gh-pages"');
 exec('git filter-branch --subdirectory-filter _gh_pages/ -f');
