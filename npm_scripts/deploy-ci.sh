e4d8559fd4a1863e9d929dfdcf32142affa58f07
Adam Armour <adam.armour@gmail.com>
2016-01-06 13:56:03 -0700

    chore(deploy-ci): only build docs if branch is deployable

--- npm_scripts/deploy-ci.sh
+++ npm_scripts/deploy-ci.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -e
 
-npm run build-docs
-
 # Do not deploy if this build is for a pull request
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "This build is for a pull request. Skipping deployment.";
-  exit 0;
+  echo "This build is for a pull request. Skipping deployment."
+  exit 0
 fi
 
 current_branch=$TRAVIS_BRANCH
 is_deployable="^v[0-9]+"
 
 if [[ $current_branch =~ $is_deployable ]]; then
+  npm run build-docs
   npm run deploy-wip -- -y $TRAVIS_BRANCH
 else
-  echo "This is not a primary branch. Skipping deployment.";
+  echo "This is not a primary branch. Skipping deployment."
 fi
