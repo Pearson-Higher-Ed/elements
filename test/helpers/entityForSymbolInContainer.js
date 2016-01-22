9ebe9bc7cbc18226cceb3d8953ad7d8c614d048f
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-01-22 15:31:43 -0700

    style(,): format changes
    space correction in beforeEach method and replace var with let

--- test/helpers/entityForSymbolInContainer.js
+++ test/helpers/entityForSymbolInContainer.js
@@ -1,8 +1,8 @@
 (global => {
 	global.entityForSymbolInContainer = (character) => {
 		
-		var code = character.replace(/"/g, '').charCodeAt(0);
-        var codeHex = code.toString(16);
+		let code = character.replace(/"/g, '').charCodeAt(0);
+        let codeHex = code.toString(16);
         
         while (codeHex.length < 4) {
             codeHex = "0" + codeHex;
--- test/spec/icons.js
+++ test/spec/icons.js
@@ -1,12 +1,15 @@
 describe('icons',() => {
+
+	let character,content;
+
 	afterEach(()=>{
-		fixture.cleanup();
+	  fixture.cleanup();
 	});
 
 	describe('pe-icon--',() =>{
 
 		beforeEach(() => {
-		fixture.load('icons/icons.html');
+		  fixture.load('icons/icons.html');
 		});
 
 		it('should match the design spec where it is pe-icon--',()=>{
@@ -26,78 +29,78 @@ describe('icons',() => {
 	describe('icons',() =>{
 
 		beforeEach(() => {
-		fixture.load('icons/icons.html');
+	      fixture.load('icons/icons.html');
 		});
-
+		
 		it('should match the design spec where it is check',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--check'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--check'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f00c");
 		});	
 
 		it('should match the design spec where it is chevron-down',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--chevron-down'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--chevron-down'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f078");
 		});
 
 		it('should match the design spec where it is chevron-up',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--chevron-up'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--chevron-up'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f077");
 		});
 
 		it('should match the design spec where it is cog',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--cog'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--cog'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f013");
 		});
 
 		it('should match the design spec where it is envelope',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--envelope'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--envelope'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f0e0");
 		});
 
 		it('should match the design spec where it is plus-circle',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--plus-circle'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--plus-circle'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f055");
 		});
 
 		it('should match the design spec where it is search',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--search'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--search'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f002");
 		});
 
 		it('should match the design spec where it is thumb-tack',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--thumb-tack'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--thumb-tack'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f08d");
 		});
 
 		it('should match the design spec where it is times',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--times'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--times'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f00d");
 		});
 
 		it('should match the design spec where it is times-circle',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--times-circle'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--times-circle'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f057");
 		});
 
 		it('should match the design spec where it is trash-o',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--trash-o'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--trash-o'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f014");
 		});
 
 		it('should match the design spec where it is users',()=>{
-			var character = window.getComputedStyle(document.querySelector('.pe-icon--users'),':before').getPropertyValue('content');
-			var content=`\\`+entityForSymbolInContainer(character);
+			character = window.getComputedStyle(document.querySelector('.pe-icon--users'),':before').getPropertyValue('content');
+			content=`\\`+entityForSymbolInContainer(character);
 			expect(content).to.equal("\\f0c0");
 		});
 
