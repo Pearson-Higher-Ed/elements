1d45334a75edba864bbddadc5f064c78b567d040
Mike <Mhomolak@yahoo.com>
2017-09-12 11:09:03 -0600

    fix: Revert deploy script

---
+++ npm_scripts/deploy-ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# Do not deploy if this build is for a pull request
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This build is for a pull request. Skipping deployment."
+  exit 0
+fi
+
+current_branch=$TRAVIS_BRANCH
+is_deployable="^v[0-9]+"
+
+if [[ $current_branch =~ $is_deployable ]]; then
+  npm run build-docs
+  npm run deploy-wip -- -y $TRAVIS_BRANCH
+else
+  echo "This is not a primary branch. Skipping deployment."
+fi
